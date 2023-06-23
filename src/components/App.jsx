1f618f84e0cde00fd895b235aaeb96a7a34457ee
Iryna Sinkevych <45698707+sugarSloth@users.noreply.github.com>
2023-06-23 19:01:28 +0300

    First attempt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import Section from "./Section/Section";
+import Statistics from "./Statistics/Statistics";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleClick = (key) => {
+    this.setState(prevState => { 
+      return {[key]: prevState[key] + 1}})  
+  }
+
+  countTotalFeedback = () => {
+      return Object.values(this.state).reduce((acc, el) => acc + el, 0 )
+  }
+
+
+  countPositiveFeedbackPercentage = () => {
+      return Math.round(100 / (this.countTotalFeedback()) * this.state.good)
+  }
+
+  render () {
+    const options = Object.keys(this.state);
+    return (
+    <>
+    <Section 
+      title="Please leave feedback"
+      children={
+        <FeedbackOptions 
+          props={options}
+          handleClick={this.handleClick}
+        />}
+    />
+
+    <Section title="Statistics" children={
+      < Statistics 
+        props={this.state}
+        total={this.countTotalFeedback}
+        percentage={this.countPositiveFeedbackPercentage}
+      />}
+    />
+    </>
+    )
+  }
+
+
+}
